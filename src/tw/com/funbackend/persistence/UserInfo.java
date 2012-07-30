c4c8da66929f310eff85d8f0a6a4664dd6868e61
AllenSu <allensu0507@gmail.com>
2012-07-30 16:37:18 +0800

    20120730

---
+++ src/tw/com/funbackend/persistence/UserInfo.java
@@ -0,0 +1,73 @@
+package tw.com.funbackend.persistence;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class UserInfo {
+	
+	/**
+	 * Mongo Id
+	 */
+	@Id
+	private String id;
+	/**
+	 * 帳號 Id
+	 */
+	private String accountId;
+	/**
+	 * 帳號密碼
+	 */
+	private String accountPass;
+	/**
+	 * 帳號名稱
+	 */
+	private String accountName;
+	/**
+	 * 最近登入時間
+	 */
+	private Date lastLoginDateTime;
+	/**
+	 * 建立時間
+	 */
+	private Date createDateTime;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public Date getLastLoginDateTime() {
+		return lastLoginDateTime;
+	}
+	public void setLastLoginDateTime(Date lastLoginDateTime) {
+		this.lastLoginDateTime = lastLoginDateTime;
+	}
+	public Date getCreateDateTime() {
+		return createDateTime;
+	}
+	public void setCreateDateTime(Date createDateTime) {
+		this.createDateTime = createDateTime;
+	}
+	public String getAccountPass() {
+		return accountPass;
+	}
+	public void setAccountPass(String accountPass) {
+		this.accountPass = accountPass;
+	}	
+}
